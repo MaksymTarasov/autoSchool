5e671141be009045c5f21747ed9b8a5417e962b3
tarasov.m <tarasovmv90@gmail.com>
2024-07-08 16:14:12 +0200

    HelloWorldTask

---
+++ src/main/java/HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+
+        printHelloName();
+    }
+
+
+    public static void printHelloName() {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Enter your name: ");
+
+
+        String name = scanner.nextLine();
+
+
+        System.out.println("Hello, " + name + "!");
+
+
+        scanner.close();
+    }
+}
